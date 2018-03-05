6248993963f2ebcb3604b17d02f3860e200e4c91
android <fakenpc>
2018-03-05 03:05:29 +0300

    acomposer, example_config

---
+++ example_config.php
@@ -0,0 +1,25 @@
+<?php
+// Add you bot's API key and name
+$bot_api_key  = '';
+$bot_username = '';
+
+// Define all paths for your custom commands in this array (leave as empty array if not used)
+$commands_paths = [
+  __DIR__ . '/Commands/',
+];
+
+// Enter your MySQL database credentials
+$mysql_credentials = [
+    'host'     => 'localhost',
+    'user'     => 'root',
+    'password' => '',
+    'database' => 'examplebot',
+];
+
+// Define the URL to your hook.php file
+$hook_url = 'https://your-domain/path/to/hook.php';
+
+// path to public key of self-signed certificate
+$certificate_path = 'webhook_cert.pem';
+
+?>

05b1c1ab09d0b1e4d326d738cb9a0953fb81db8d
android <fakenpc>
2018-03-15 06:35:58 +0300

    временно убираем блок по ip

--- free-kassa.php
+++ free-kassa.php
@@ -37,7 +37,7 @@ try {
 
 	
 	if (!in_array(getIP(), array('136.243.38.147', '136.243.38.149', '136.243.38.150', '136.243.38.151', '136.243.38.189', '88.198.88.98'))) {
-		die("hacking attempt!");
+		// die("hacking attempt!");
 	}
 
 	$sign = md5($merchant_id.':'.$_REQUEST['AMOUNT'].':'.$merchant_secret_response.':'.$_REQUEST['MERCHANT_ORDER_ID']);
